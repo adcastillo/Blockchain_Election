9f84562d4862554613d9a4ff87575d7eaa46eeae
Angel Castillo <AngelCastillo@Angels-MacBook-Pro.local>
2018-08-30 10:52:37 -0500

    Election fil

---
+++ election/contracts/Election.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+	//Store candidate
+	//read candidate
+	string public candidate;
+
+	//Constructor 
+	function Election () public{
+		candidate = "Candidate 1";
+
+
+	}
+
+
+
+
+}
